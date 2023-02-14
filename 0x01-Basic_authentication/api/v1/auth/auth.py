142c6f7159653a09ffe6fab7c166e68639c9fa41
keimah507 <githinjibrian5@gmail.com>
2023-02-14 16:27:23 +0300

    upload files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
@@ -32,3 +33,4 @@ class Auth:
         """Current user method
         """
         return None
+
