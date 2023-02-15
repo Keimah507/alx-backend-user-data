165cbb2dcbb70a0818ceba6bc06389a81b5b2173
keimah507 <githinjibrian5@gmail.com>
2023-02-15 09:03:12 +0300

    Add method user_id_for_session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,7 @@ class SessionAuth(Auth):
         super().__init__(Auth)
         self.user_id_by_session_id = {}
 
-    
+
     def create_session(self, user_id: str = None) -> str:
         """Creates a session id for user_id"""
         if user_id is None and user_id is not str:
@@ -17,3 +17,10 @@ class SessionAuth(Auth):
         self.session_id = str(uuid.uuid4())
         self.user_id_by_session_id = {self.session_id: user_id}
         return self.session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a user id based on session id"""
+        if session_id is None and session_id is not str:
+            return None
+        return user_id_by_session_id.get(session_id)
+
